0342a7585417c6b3876aceb888409d8e8e59ce7d
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-05-21 19:53:31 -0500

    Change product API URL to production
    Updated API endpoint for fetching product data.

--- src/pages/Scanner.js
+++ src/pages/Scanner.js
@@ -12,7 +12,7 @@ export default function Scanner() {
     try {
       setError("");
       const res = await axios.get(
-        `http://localhost:4000/api/productos/${codigo}`
+        `https://backinventario-wns5.onrender.com/api/productos/${codigo}`
       );
       setProducto(res.data);
       setScanning(false);
