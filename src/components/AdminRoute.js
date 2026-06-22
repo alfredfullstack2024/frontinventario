6cf4cb92d8fc934986653e0320242400713b7d92
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-06-22 15:49:15 -0500

    Create AdminRoute.js

---
+++ src/components/AdminRoute.js
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+export default function AdminRoute({ children }) {
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+  if (!usuario) {
+    return <Navigate to="/login" />;
+  }
+
+  if (usuario.rol !== "admin") {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
