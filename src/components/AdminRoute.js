5ad5679593c273f533e81f6084d02c5367f71ae2
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-06-22 16:33:23 -0500

    Update AdminRoute.js

--- src/components/AdminRoute.js
+++ src/components/AdminRoute.js
@@ -3,12 +3,14 @@ import { Navigate } from "react-router-dom";
 export default function AdminRoute({ children }) {
   const usuario = JSON.parse(localStorage.getItem("usuario"));
 
+  // Si no ha iniciado sesión
   if (!usuario) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
+  // Si no es administrador
   if (usuario.rol !== "admin") {
-    return <Navigate to="/" />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
