fe9094aabd676c842cbc0d23136db5393a4c17d5
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-06-24 16:11:06 -0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
-  const navigate = useNavigate();
+  
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
